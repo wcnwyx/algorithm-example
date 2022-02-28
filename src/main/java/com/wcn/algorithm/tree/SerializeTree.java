3d52a3c935420429e5014bc7afc99ea867da2801
wucheng <wc@candypay.com>
2022-02-28 14:27:37 +0800

    tree serialize

--- src/main/java/com/wcn/algorithm/tree/SerializeTree.java
+++ src/main/java/com/wcn/algorithm/tree/SerializeTree.java
@@ -15,13 +15,22 @@ public class SerializeTree {
         node1.left = node2;
         node2.right = node3;
 
-        Queue<Integer> queue = serialize(node1);
+        Queue<Integer> queue = serializeBefore(node1);
         while(!queue.isEmpty()){
             Integer i = queue.poll();
             System.out.print(i+" ");
         }
 
-        Node node = serializeFrom(serialize(node1));
+        Node node = serializeFromBefore(serializeBefore(node1));
+        System.out.println();
+
+        queue = serializeWide(node1);
+        while(!queue.isEmpty()){
+            Integer i = queue.poll();
+            System.out.print(i+" ");
+        }
+
+        node = serializeFromWide(serializeWide(node1));
         System.out.println();
     }
 
@@ -39,21 +48,21 @@ public class SerializeTree {
      * 将树序列化(先序)
      * @return
      */
-    public static Queue<Integer> serialize(Node root){
+    public static Queue<Integer> serializeBefore(Node root){
         Queue<Integer> queue = new LinkedList<>();
-        process(root, queue);
+        processBefore(root, queue);
         return queue;
     }
 
-    private static void process(Node node, Queue<Integer> queue){
+    private static void processBefore(Node node, Queue<Integer> queue){
         if(node==null){
             queue.add(null);
             return ;
         }
         queue.offer(node.value);
 
-        process(node.left, queue);
-        process(node.right, queue);
+        processBefore(node.left, queue);
+        processBefore(node.right, queue);
     }
 
     /**
@@ -61,17 +70,73 @@ public class SerializeTree {
      * @param queue
      * @return
      */
-    public static Node serializeFrom(Queue<Integer> queue){
+    public static Node serializeFromBefore(Queue<Integer> queue){
         Integer value = queue.poll();
         if(value==null){
             return null;
         }
         Node node = new Node(value);
-        Node left = serializeFrom(queue);
-        Node right = serializeFrom(queue);
+        Node left = serializeFromBefore(queue);
+        Node right = serializeFromBefore(queue);
         node.left = left;
         node.right = right;
         return node;
     }
 
+    /**
+     * 序列化（宽度优先）
+     * @param root
+     * @return
+     */
+    public static Queue<Integer> serializeWide(Node root){
+        Queue<Integer> queue = new LinkedList<>();
+        Queue<Node> nodeQueue = new LinkedList<>();
+        nodeQueue.offer(root);
+        queue.offer(root.value);
+        while(!nodeQueue.isEmpty()){
+            Node node = nodeQueue.poll();
+            if(node.left!=null){
+                nodeQueue.offer(node.left);
+                queue.offer(node.left.value);
+            }else{
+                queue.offer(null);
+            }
+
+            if(node.right!=null){
+                nodeQueue.offer(node.right);
+                queue.offer(node.right.value);
+            }else{
+                queue.offer(null);
+            }
+        }
+        return queue;
+    }
+
+    /**
+     * 反序列化（宽度优先）
+     * @param queue
+     * @return
+     */
+    public static Node serializeFromWide(Queue<Integer> queue){
+        Queue<Node> nodeQueue = new LinkedList<>();
+        Node root = new Node(queue.poll());
+        nodeQueue.offer(root);
+        Node nodeTemp = null;
+        while(!nodeQueue.isEmpty()){
+            nodeTemp = nodeQueue.poll();
+
+            Integer valueLeft = queue.poll();
+            if(valueLeft!=null){
+                nodeTemp.left = new Node(valueLeft);
+                nodeQueue.offer(nodeTemp.left);
+            }
+
+            Integer valueRight = queue.poll();
+            if(valueRight!=null){
+                nodeTemp.right = new Node(valueRight);
+                nodeQueue.offer(nodeTemp.right);
+            }
+        }
+        return root;
+    }
 }
