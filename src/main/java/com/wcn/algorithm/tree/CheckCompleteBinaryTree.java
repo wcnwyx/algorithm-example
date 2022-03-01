a077bcdde2c426cf4ecd8a31a311f427e8df0896
wucheng <wc@candypay.com>
2022-03-01 15:39:55 +0800

    check completeBinaryTree

--- src/main/java/com/wcn/algorithm/tree/CheckCompleteBinaryTree.java
+++ src/main/java/com/wcn/algorithm/tree/CheckCompleteBinaryTree.java
@@ -14,15 +14,18 @@ import java.util.Queue;
 public class CheckCompleteBinaryTree {
     public static void main(String[] args) {
         Node node1 = new Node(1);
-        System.out.println(check(node1));
+        System.out.println(check(node1)+" "+checkLoop(node1));
 
         Node node2 = new Node(2);
         node1.right = node2;
-        System.out.println(check(node1));
+        System.out.println(check(node1)+"  "+checkLoop(node1));
 
         node1.left = node2;
-        System.out.println(check(node1));
+        System.out.println(check(node1)+"  "+checkLoop(node1));
 
+        Node node3 = new Node(3);
+        node1.right = node3;
+        System.out.println(check(node1)+"  "+checkLoop(node1));
     }
 
     static class Node{
@@ -35,6 +38,11 @@ public class CheckCompleteBinaryTree {
         }
     }
 
+    /**
+     * 宽度遍历实现
+     * @param root
+     * @return
+     */
     public static boolean check(Node root){
         //利用树的宽度遍历，判断非空节点中间是否夹着空节点
         Queue<Node> queue = new LinkedList<>();
@@ -54,5 +62,52 @@ public class CheckCompleteBinaryTree {
             }
         }
         return true;
+    }
+
+    /**
+     * 递归实现
+     * @param root
+     * @return
+     */
+    public static boolean checkLoop(Node root){
+        return process(root).isCBT;
+    }
+
+    public static Result process(Node node){
+        if(node==null){
+            return new Result(0, true, true);
+        }
+        Result leftResult = process(node.left);
+        Result rightResult = process(node.right);
+
+        int height = Math.max(leftResult.height, rightResult.height) + 1;
+        boolean isFBT = false;
+        boolean isCBT = false;
+        if(leftResult.isFBT && rightResult.isFBT && leftResult.height== rightResult.height){
+            //子树都是满二叉树，且高度一样，则该节点的树肯定是满二叉树
+            isFBT = true;
+            isCBT = true;
+        }else if(leftResult.isFBT && rightResult.isCBT && leftResult.height==rightResult.height){
+            //左为满二叉树，右为完全二叉树，且高度相等，则该节点的树为完全二叉树，但不是满二叉树
+            isFBT = false;
+            isCBT = true;
+        }else if(leftResult.isCBT && rightResult.isFBT && leftResult.height - rightResult.height==1){
+            //左为完全二叉树，右为满二叉树，左树比右树高度大1，则node为完全二叉树，但不是满二叉树
+            isFBT = false;
+            isCBT = true;
+        }
+        return new Result(height, isFBT, isCBT);
+    }
+
+    static class Result{
+        int height;
+        boolean isFBT;//是否是满二叉树
+        boolean isCBT;//是否是完全二叉树
+
+        public Result(int height, boolean isFBT, boolean isCBT) {
+            this.height = height;
+            this.isFBT = isFBT;
+            this.isCBT = isCBT;
+        }
     }
 }
