f75cef2db8f58013195bf9be27b3cd80612db109
wucheng <wc@candypay.com>
2022-03-03 15:20:45 +0800

    graph

--- src/main/java/com/wcn/algorithm/graph/ScanDeep.java
+++ src/main/java/com/wcn/algorithm/graph/ScanDeep.java
@@ -11,9 +11,9 @@ import java.util.Stack;
  */
 public class ScanDeep {
     public static void main(String[] args) {
-        int[][] array = new int[][]{{1,2,0},{1,3,0},{2,3,0},{2,4,0},{3,5,0},{4,5,0}};
+        int[][] array = new int[][]{{1,2,0},{1,3,0},{2,3,0},{2,4,0},{3,5,0},{5,6,0},{6,7,0},{3,4,0}};
         Graph graph = new Graph(array);
-        scan(graph.nodes.get(1));
+        scan1(graph.nodes.get(1));
     }
 
     public static void scan(GraphNode node){
@@ -38,6 +38,29 @@ public class ScanDeep {
                     System.out.println(nodeTempNext.value);
 
                     break;//只压入一个子节点就跳出
+                }
+            }
+        }
+    }
+
+    public static void scan1(GraphNode node){
+        if(node==null){
+            return ;
+        }
+
+        //栈中保存着当前节点所有的路径节点
+        Stack<GraphNode> stack = new Stack<>();
+        HashSet<GraphNode> hashSet = new HashSet<>();
+        stack.push(node);
+        hashSet.add(node);
+        while(!stack.isEmpty()){
+            GraphNode nodeTemp = stack.pop();
+            System.out.println(nodeTemp.value);
+
+            for(GraphNode nodeTempNext:nodeTemp.nexts){
+                if(!hashSet.contains(nodeTempNext)){
+                    stack.push(nodeTempNext);//再压入本节点
+                    hashSet.add(nodeTempNext);
                 }
             }
         }
